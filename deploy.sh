422653bcb4e61688920744698ba15585b14f3824
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-02-12 21:39:03 +1100

    Add new deploy and install scripts

--- deploy.sh
+++ deploy.sh
@@ -3,38 +3,16 @@
 set -e
 set -o pipefail
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-script_filename="$(basename "${BASH_SOURCE[0]}")"
-cd $script_dir
+branch=$(git rev-parse --abbrev-ref HEAD)
+revision=$(git rev-parse --short HEAD)
 
 echo "----------"
-echo "User: $USER"
-echo "Host: $HOSTNAME"
-echo "Path: $PWD"
+echo "Deploying:"
+echo $branch
+echo $revision
 echo "----------"
-echo "Running $script_filename ..."
+echo "scp install.sh deploy@server-singapore.nabu.io:/var/www/reddust.org.au"
+scp install.sh deploy@server-singapore.nabu.io:/var/www/reddust.org.au
 echo "----------"
-echo "git pull"
-git pull
-echo "----------"
-echo "git checkout 'production' -f"
-git checkout 'production' -f
-echo "----------"
-
-# This hack makes the nvm binary available to this script.
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-echo "nvm install"
-nvm install
-echo "----------"
-echo "yarn install"
-yarn install
-echo "----------"
-echo "bower install"
-bower install
-echo "----------"
-echo "gulp build"
-gulp build
-echo "----------"
+echo 'ssh deploy@server-singapore.nabu.io "/var/www/reddust.org.au/install.sh $branch $revision"'
+ssh deploy@server-singapore.nabu.io "/var/www/reddust.org.au/install.sh $branch $revision"

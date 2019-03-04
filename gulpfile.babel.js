15d22b165875f507357156a57a8e51ffa19ff411
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-03-04 14:22:41 +1100

    Refactor to new Gulp 4 setup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,123 +1,83 @@
-"use strict";
+// Extract methods from Gulp
+// https://gulpjs.com/docs/en/api/src
+const { src, dest, series, parallel, watch } = require("gulp");
 
-// Gulp packages
-import autoprefixer from "gulp-autoprefixer";
-import babel from "gulp-babel";
-import concat from "gulp-concat";
-import connect from "gulp-connect";
-import del from "del";
-import eslint from "gulp-eslint";
-// import gitrev from "git-rev-sync";
-import gulp from "gulp";
-import header from "gulp-header";
-// import inlineCss from "gulp-inline-css";
-import minifyCSS from "gulp-clean-css";
-import noop from "gulp-noop";
-import nunjucksRender from "gulp-nunjucks-render";
-import preprocess from "gulp-preprocess";
-import prettify from "gulp-jsbeautifier";
-import rename from "gulp-rename";
-import replace from "gulp-replace";
-import sass from "gulp-sass";
-// import sitemap from "gulp-sitemap";
-import size from "gulp-size";
-import uglify from "gulp-uglify";
-import util from "gulp-util";
+// Import plugins
+// https://gulpjs.com/docs/en/getting-started/using-plugins
+const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const connect = require("gulp-connect");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const normalize = require("node-normalize-scss");
+const nunjucksRender = require("gulp-nunjucks-render");
+const prettify = require("gulp-jsbeautifier");
+const rename = require("gulp-rename");
+const sass = require("gulp-dart-sass");
+const size = require("gulp-size");
+const uglify = require("gulp-uglify");
 
-const PKG = require("./package.json");
+// Load in data
+const config = require("./package.json");
+const { contributors } = config;
+const people = require("./src/json/people.json");
+const partners = require("./src/json/partners.json");
+const musicVideosTop = require("./src/json/music-videos-top.json");
+const musicVideosBottom = require("./src/json/music-videos-bottom.json");
 
-const environment = util.env.env || "development";
-const isProduction = environment === "production";
-const buildVersion = PKG.version;
-
-const people = require("./src/templates/data/people.json");
-const partners = require("./src/templates/data/partners.json");
-const musicVideosTop = require("./src/templates/data/music-videos-top.json");
-const musicVideosBottom = require("./src/templates/data/music-videos-bottom.json");
-
-// The build aware config passed down all HTML and JS files
-const config = {
-  humanName: "Red Dust",
-  name: PKG.name,
-  homepage: PKG.homepage,
-  isProduction,
-  environment,
-  buildVersion
-};
-
-// Delete the dist folder
-gulp.task("deleteDist", () => {
+// Removes the dist folder
+function clean() {
   return del(["dist"]);
-});
+}
 
-// Delete the temp folder
-gulp.task("deleteTemp", () => {
-  return del(["temp"]);
-});
+// Copies over all files from `src/public` as they are to `dist/`
+function assets() {
+  return src("src/public/**/*").pipe(dest("dist/"));
+}
 
-// Copy over all files from public folder "as they are" to the dist folder
-gulp.task("copyPublic", () => {
-  return gulp
-    .src("src/public/**/*")
-    .pipe(gulp.dest("dist/"))
-    .pipe(connect.reload());
-});
-
-// Copy the outdatedbrowser JS
-gulp.task("copyOutdatedBrowserJs", () => {
-  return gulp
-    .src(
-      "bower_components/outdated-browser/outdatedbrowser/outdatedbrowser.min.js"
+// Compiles all the HTML
+function html() {
+  return src("src/html/pages/**/*.+(html|njk)")
+    .pipe(
+      nunjucksRender({
+        path: ["src/html"],
+        data: {
+          language: "en",
+          themeColour: "#A12022",
+          copyRight: "Copyright 2016 Red Dust Role Models Limited, Australia",
+          contributors,
+          people,
+          partners,
+          musicVideosTop,
+          musicVideosBottom
+        }
+      })
     )
-    .pipe(gulp.dest("dist/assets/js/"));
-});
-
-// Copy the outdatedbrowser CSS
-gulp.task("copyOutdatedBrowserCss", () => {
-  return gulp
-    .src(
-      "bower_components/outdated-browser/outdatedbrowser/outdatedbrowser.min.css"
+    .pipe(
+      prettify({
+        html: {
+          indent_inner_html: true,
+          indent_size: 2,
+          max_preserve_newlines: 0
+        }
+      })
     )
-    .pipe(gulp.dest("dist/assets/css/"));
-});
+    .pipe(dest("dist"));
+}
 
-// Compile all HTML
-gulp.task("html", function() {
-  return (
-    gulp
-      .src("src/templates/pages/**/*.+(html|njk)")
-      .pipe(
-        nunjucksRender({
-          path: ["src/templates"],
-          data: { config, people, partners, musicVideosTop, musicVideosBottom }
-        })
-      )
-      .pipe(prettify({ config: "./jsbeautifyrc.json" }))
-      .pipe(gulp.dest("dist"))
-      // .pipe(
-      //   sitemap({
-      //     siteUrl: config.homepage,
-      //     changefreq: "monthly",
-      //     priority: 0.5
-      //   })
-      // )
-      .pipe(gulp.dest("dist"))
-      .pipe(connect.reload())
-  );
-});
-
-// Compile all CSS
-gulp.task("css", () => {
-  return gulp
-    .src("src/styles/**/*.scss")
+// Compiles all the CSS
+function css() {
+  return src("src/css/app.scss")
     .pipe(
       sass({
-        outputStyle: "expanded",
-        includePaths: require("node-normalize-scss").includePaths
+        outputStyle: "compressed",
+        includePaths: normalize.includePaths
       }).on("error", sass.logError)
     )
     .pipe(
       autoprefixer({
+        cascade: false,
         browsers: [
           "> 1% in AU",
           "Explorer > 9",
@@ -125,136 +85,85 @@ gulp.task("css", () => {
           "Chrome >= 10",
           "Safari >= 6",
           "iOS >= 6"
-        ],
-        cascade: false
+        ]
       })
     )
-    .pipe(minifyCSS({ keepSpecialComments: "none" }))
+    .pipe(rename("app.min.css"))
+    .pipe(dest("dist/assets/css"));
+}
+
+// Check your JS syntax against ES Lint
+function lintJs() {
+  return src("src/js/app.js")
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Convert your JS with Babel and minify it
+function buildJs() {
+  return src("src/js/app.js")
     .pipe(
-      rename({
-        basename: config.name,
-        extname: ".min.css"
+      babel({
+        presets: ["@babel/env"]
       })
     )
-    .pipe(gulp.dest("dist/assets/css"))
-    .pipe(connect.reload());
-});
+    .pipe(uglify())
+    .pipe(rename("app.min.js"))
+    .pipe(dest("dist/assets/js"));
+}
 
-// Lint all JS files, warn about bad JS, break on errors
-// gulp.task("lintJs", () => {
-//   return gulp
-//     .src(["src/app.js"])
-//     .pipe(eslint())
-//     .pipe(eslint.format());
-// });
+// Concatenate all vendor JS
+function concatVendorJs() {
+  return src([
+    "bower_components/jquery/dist/jquery.min.js",
+    "bower_components/velocity/velocity.min.js",
+    "bower_components/countUp.js/dist/countUp.min.js",
+    "src/js/vendor/google-analytics.js"
+  ])
+    .pipe(concat("vendor.min.js"))
+    .pipe(dest("dist/assets/js"));
+}
 
-// Compile all JS
-gulp.task("appJs", () => {
-  const buildDate = new Date().toUTCString();
-  // const buildHash = gitrev.long();
-  const legalBanner = [
-    "/**",
-    `* Red Dust v${buildVersion}`,
-    "* https://www.hotdoc.com.au/",
-    "* Copyright Red Dust Role Models Limited, Australia",
-    `* Build date: ${buildDate}`,
-    // `* Build hash: ${buildHash}`,
-    " */",
-    ""
-  ].join("\n");
-  const context = config;
-  return gulp
-    .src([`src/js/app.js`])
-    .pipe(preprocess({ context }))
-    .pipe(babel())
-    .pipe(isProduction ? uglify({ preserveComments: "license" }) : noop())
-    .pipe(header(legalBanner))
-    .pipe(
-      rename({
-        extname: ".min.js"
-      })
-    )
-    .pipe(gulp.dest("temp/js/"));
-});
-
-gulp.task("vendorJs", () => {
-  return gulp
-    .src([
-      "bower_components/jquery/dist/jquery.min.js",
-      "bower_components/velocity/velocity.min.js",
-      "bower_components/countUp.js/dist/countUp.min.js",
-      "temp/js/app.min.js"
-    ])
-    .pipe(
-      concat(`${config.name}.min.js`),
-      { newLine: "\n\n\n\n" }
-    )
-    .pipe(replace(/^\s*\r?\n/gm, ""))
-    .pipe(gulp.dest("dist/assets/js"));
-});
-
-// Live reload JS files in browser
-gulp.task("reloadJs", () => {
-  return gulp.src(["dist/assets/js/**/*.js"]).pipe(connect.reload());
-});
-
-// Build all JS files
-gulp.task("js", gulp.series("appJs", "vendorJs", "reloadJs"));
-
-// Watch all files and run tasks when files change
-gulp.task("watch", () => {
-  gulp.watch(["src/public/**/*"], gulp.parallel("copyPublic"));
-  gulp.watch(["src/templates/**/*.+(html|njk|json)"], gulp.parallel("html"));
-  gulp.watch(["src/styles/**/*.scss"], gulp.parallel("css"));
-  gulp.watch(["src/js/**/*.js", ".babelrc", ".eslintrc"], gulp.parallel("js"));
-  gulp.watch(
-    ["gulpfile.babel.js", "package.json", "bower.json"],
-    gulp.series(["build"])
+// Reports an overview of the `dist/` folder
+function report() {
+  return src(["dist/**/*"]).pipe(
+    size({
+      showFiles: true,
+      showTotal: false
+    })
   );
-});
+}
 
-// Run a local server on http://localhost:9000
-gulp.task("serve", () => {
+// Spins up a localhost server on http://localhost:9000
+function localhost() {
   connect.server({
     root: "dist",
-    livereload: true,
     port: 9000
   });
-});
+}
 
-// Report all file sizes
-gulp.task("report", () => {
-  return gulp
-    .src([
-      "dist/**/*",
-      "!dist/assets/favicons/**",
-      "!dist/assets/fonts/**",
-      "!dist/assets/img/**"
-    ])
-    .pipe(
-      size({
-        showFiles: true,
-        showTotal: false
-      })
-    );
-});
+// Watches the `src/` folder for file changes and fires tasks accordingly
+// https://gulpjs.com/docs/en/getting-started/watching-files
+function watchers() {
+  watch("src/public/**/*", assets);
+  watch("src/html/**/*.njk", html);
+  watch("src/css/**/*.scss", css);
+  watch("src/js/**/*.js", js);
+}
 
-// Build the entire dist folder
-gulp.task(
-  "build",
-  gulp.series(
-    "deleteDist",
-    gulp.parallel(
-      "html",
-      "css",
-      "js",
-      "copyPublic",
-      "copyOutdatedBrowserJs",
-      "copyOutdatedBrowserCss"
-    ),
-    gulp.parallel("deleteTemp", "report")
-  )
-);
+// Create Gulp commands
+// https://gulpjs.com/docs/en/getting-started/creating-tasks
+const js = parallel(concatVendorJs, series(lintJs, buildJs));
+const build = series(clean, parallel(assets, html, css, js), report);
+const serve = series(build, parallel(localhost, watchers));
 
-// Default gulp command
-gulp.task("default", gulp.series("build", gulp.parallel("watch", "serve")));
+// Finally make those tasks available in Gulp CLI
+exports.build = build;
+exports.serve = serve;
+
+// For testing
+exports.css = css;
+exports.html = html;
+exports.js = js;
+exports.assets = assets;

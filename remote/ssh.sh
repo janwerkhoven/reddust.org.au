e216ff197822d3a63956446b7b16685c136691d7
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-07-26 17:18:11 +1000

    Correct the permissions on id_rsa file

--- remote/ssh.sh
+++ remote/ssh.sh
@@ -8,7 +8,7 @@ echo "Preparing SSH:"
 echo "----------"
 (set -x; mkdir ~/.ssh)
 echo "----------"
-(set -x; chmod -R 0700 ~/.ssh)
+(set -x; chmod -R 0600 ~/.ssh)
 echo "----------"
 (set -x; ssh-keyscan -H singapore.server.floatplane.dev > ~/.ssh/known_hosts)
 echo "----------"
